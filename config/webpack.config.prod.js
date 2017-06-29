d231af816a5533aa0880d472b0c088a0be900a55
Abdul Dakkak <abduld@wolfram.com>
2017-06-29 19:02:00 +0100

    play...

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -88,7 +88,7 @@ module.exports = {
     // for React Native Web.
     extensions: ['.web.js', '.js', '.json', '.web.jsx', '.jsx'],
     alias: {
-      
+
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
@@ -118,7 +118,7 @@ module.exports = {
           {
             options: {
               formatter: eslintFormatter,
-              
+
             },
             loader: require.resolve('eslint-loader'),
           },
@@ -165,7 +165,7 @@ module.exports = {
         include: paths.appSrc,
         loader: require.resolve('babel-loader'),
         options: {
-          
+
           compact: true,
         },
       },
@@ -194,6 +194,8 @@ module.exports = {
                     importLoaders: 1,
                     minimize: true,
                     sourceMap: true,
+                    modules: true,
+                    localIdentName: "[name]__[local]___[hash:base64:5]"
                   },
                 },
                 {
--- src/stories/CodeEditor.js
+++ src/stories/CodeEditor.js
@@ -1,9 +1,52 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import CodeEditor from "../Components/CodeEditor";
+import CodeEditor from "components/CodeEditor";
+import yeast from "yeast";
 
-import vectorAddCode from "!!raw!./_fixtures/vectoradd.cu";
+import vectorAddCode from "!raw-loader!./_fixtures/vectoradd.cu";
 
 storiesOf("CodeEditor", module)
-  .add("default", () => <CodeEditor content={"Hello world"} />)
-  .add("cuda", () => <CodeEditor content={vectorAddCode} />);
+  .add("default", () =>
+    <CodeEditor
+      files={{
+        code: { content: "Hello", createdOn: new Date(), uuid: yeast() }
+      }}
+    />
+  )
+  .add("cuda", () =>
+    <CodeEditor
+      files={{
+        code: { content: vectorAddCode, createdOn: new Date(), uuid: yeast() }
+      }}
+    />
+  )
+  .add("cuda-with-menu", () =>
+    <CodeEditor
+      withMenuBar
+      files={{
+        code: { content: vectorAddCode, createdOn: new Date(), uuid: yeast() }
+      }}
+      onSaveIconClick={(_, v) => console.log(v)}
+    />
+  )
+  .add("multifile", () =>
+    <CodeEditor
+      withMenuBar
+      currentFile={"file1"}
+      files={{
+        file1: { content: vectorAddCode, createdOn: new Date(), uuid: yeast() },
+        file2: { content: vectorAddCode, createdOn: new Date(), uuid: yeast() }
+      }}
+      onSaveIconClick={(_, v) => console.log(v)}
+    />
+  )
+  .add("multifile-nobar", () =>
+    <CodeEditor
+      currentFile={"file1"}
+      files={{
+        file1: { content: vectorAddCode, createdOn: new Date(), uuid: yeast() },
+        file2: { content: vectorAddCode, createdOn: new Date(), uuid: yeast() }
+      }}
+      onSaveIconClick={(_, v) => console.log(v)}
+    />
+  );
