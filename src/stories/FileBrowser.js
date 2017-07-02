150799811c824ff081672549e89b8366936b30cc
Abdul Dakkak <abduld@wolfram.com>
2017-07-02 13:20:06 +0100

    work

--- src/stories/FileBrowser.js
+++ src/stories/FileBrowser.js
@@ -1,59 +1,56 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import FileBrowser from "components/FileBrowser";
-import { Page } from "hedron";
 import yeast from "yeast";
 
 storiesOf("FileBrowser", module).add("default", () =>
-  <Page>
-    <FileBrowser
-      content={[
-        {
-          name: "dir1",
-          isDirectory: true,
-          uuid: yeast(),
-          content: [
-            {
-              name: "test1.txt",
-              url: "test1.txt",
-              uuid: yeast()
-            },
-            {
-              name: "test2.txt",
-              url: "test2.txt",
-              uuid: yeast()
-            },
-            {
-              name: "test3.txt",
-              url: "test3.txt",
-              uuid: yeast()
-            },
-            {
-              name: "test4.txt",
-              url: "test4.txt",
-              uuid: yeast()
-            },
-            {
-              name: "test5.txt",
-              url: "test5.txt",
-              uuid: yeast()
-            }
-          ]
-        },
-        {
-          name: "dir2",
-          uuid: yeast(),
-          isDirectory: true,
-          isOpen: true,
-          content: [
-            {
-              name: "test.txt",
-              url: "test.txt",
-              uuid: yeast()
-            }
-          ]
-        }
-      ]}
-    />
-  </Page>
+  <FileBrowser
+    content={[
+      {
+        name: "dir1",
+        isDirectory: true,
+        uuid: yeast(),
+        content: [
+          {
+            name: "test1.txt",
+            url: "test1.txt",
+            uuid: yeast()
+          },
+          {
+            name: "test2.txt",
+            url: "test2.txt",
+            uuid: yeast()
+          },
+          {
+            name: "test3.txt",
+            url: "test3.txt",
+            uuid: yeast()
+          },
+          {
+            name: "test4.txt",
+            url: "test4.txt",
+            uuid: yeast()
+          },
+          {
+            name: "test5.txt",
+            url: "test5.txt",
+            uuid: yeast()
+          }
+        ]
+      },
+      {
+        name: "dir2",
+        uuid: yeast(),
+        isDirectory: true,
+        isOpen: true,
+        content: [
+          {
+            name: "test.txt",
+            url: "test.txt",
+            uuid: yeast()
+          }
+        ]
+      }
+    ]}
+  />
 );
